5054678e5291987a0583f7f873a7d74029520c24
Hayden50 <haydenrieder50@hotmail.com>
2022-12-19 16:51:09 -0500

    webserver successfully prints hello

--- src/main.c
+++ src/main.c
@@ -3,22 +3,36 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/socket.h>
-#include "socket.c"
+// #include "../headers/socket.h"
 #define PORT 8123
 
 int main() {
   
   struct sockaddr_in addr_in;
-  int sockfd = create_socket(&addr_in, PORT);
-  int addr_size = sizeof(addr_in);
-  char *res = "Hello World!";
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  addr_in.sin_family = AF_INET;
+  addr_in.sin_addr.s_addr = INADDR_ANY;
+  addr_in.sin_port = htons(PORT);
+
+  if (bind(sockfd, (struct sockaddr *) &addr_in, sizeof(addr_in)) < 0) {
+    printf("Bind Error");
+  }
+
+  if (listen(sockfd, 5) < 0) {
+    printf("Listen Error");
+  }
+  
+  char *res = "HTTP/1.1 200 OK\r\n\r\nHello";
 
   while (1) {
-    char buffer[1024] = { 0 };
-    int new_socket = accept(sockfd, (struct sockaddr*)&addr_in, (socklen_t*)&addr_size);
+    char buffer[1024] = "";
+    int new_socket = accept(sockfd, NULL, NULL);
     read(new_socket, buffer, 1023);
-    printf("%s\n", buffer);
-    send(new_socket, res, strlen(res), 0);
+    printf("Response: %s\n", buffer);
+    write(new_socket, res, strlen(res));
+    close(new_socket);
   }
+
   return 0;
 }
